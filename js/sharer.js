b6e1ea7f1abf10cd696e6f164e73ee22648b4385
shivarajnaidu <shivarajnaidu@gmail.com>
2016-06-24 12:09:07 +0530

    stumbleupon added to sharing list

--- js/sharer.js
+++ js/sharer.js
@@ -13,42 +13,47 @@ This program is free software: you can redistribute it and/or modify
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-document.addEventListener('DOMContentLoaded', function() {
-  'use strict';
+document.addEventListener("DOMContentLoaded", function() {
+  "use strict";
 
 function lit(tabUrl) {
     function urlAssigner(link_) {
-      let id = '#'+link_.id;
+      let id = "#"+link_.id;
       let a = document.querySelector(id);
       a.href = link_.href;
-      a.target ='_blank';
+      a.target ="_blank";
     }
     
     let facebook = {};
-    facebook.href = 'https://www.facebook.com/sharer.php?u=' + tabUrl;
-    facebook.id : 'uv-facebook';
+    facebook.href = "https://www.facebook.com/sharer.php?u=" + tabUrl;
+    facebook.id = "uv-facebook";
 
     let twitter = {};
-    twitter.href = 'https://twitter.com/share?url=' + tabUrl;
-    twitter.id = 'uv-twitter';
+    twitter.href = "https://twitter.com/share?url=" + tabUrl;
+    twitter.id = "uv-twitter";
 
     let googlePlus = {};
-    googlePlus.href = 'https://plus.google.com/share?url=' + tabUrl;
-    googlePlus.id = 'uv-google-plus';
+    googlePlus.href = "https://plus.google.com/share?url=" + tabUrl;
+    googlePlus.id = "uv-google-plus";
 
     let reddit = {};
-    reddit.href = 'https://www.reddit.com/submit?url=' + tabUrl;
-    reddit.id = 'uv-reddit';
+    reddit.href = "https://www.reddit.com/submit?url=" + tabUrl;
+    reddit.id = "uv-reddit";
 
     let linkedin = {};
-    linkedin.href = 'https://www.linkedin.com/shareArticle?url=' + tabUrl;
-    linkedin.id = 'uv-linkedin';
+    linkedin.href = "https://www.linkedin.com/shareArticle?url=" + tabUrl;
+    linkedin.id = "uv-linkedin";
+
+    let stumbleupon = {};
+    stumbleupon.href = "http://www.stumbleupon.com/submit?url=" + tabUrl;
+    stumbleupon.id = "uv-stumbleupon";
 
     let facebookButton = urlAssigner(facebook);
     let googleplusButton = urlAssigner(googlePlus);
     let twitterButton = urlAssigner(twitter);
     let redditButton = urlAssigner(reddit);
-    let linkedinButton = urlAssigner(linkedin);    
+    let linkedinButton = urlAssigner(linkedin); 
+    let stumbleuponButton = urlAssigner(stumbleupon);   
 }
 
   (function getCurrentTabUrl() {
